9cca518c9b248ddf15a751e3f5441d3e07bc08d5
jmseca <joao.m.fonseca@gmail.com>
2024-01-20 22:59:38 +0000

    [Fix] helper files
    More deprecated names

--- lvgl_helpers.c
+++ lvgl_helpers.c
@@ -101,7 +101,8 @@ void lvgl_driver_init(void)
 
     lvgl_spi_driver_init(TFT_SPI_HOST,
         DISP_SPI_MISO, DISP_SPI_MOSI, DISP_SPI_CLK,
-        SPI_BUS_MAX_TRANSFER_SZ, 1,
+        3*40*240, //SPI_BUS_MAX_TRANSFER_SZ,
+        1, //DMA
         DISP_SPI_IO2, DISP_SPI_IO3);
 
     disp_spi_add_device(TFT_SPI_HOST);
